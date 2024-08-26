e3b865876fea32fc68cef081180bffcd3a39aa46
Ömer ATABER <67821076+OmerAti@users.noreply.github.com>
2024-08-26 13:45:09 +0300

    Update config.php

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ return [
         'author'       => "\xc3\x96mer ATABER - OmerAti JRodix.Com Internet Hizmetleri",
         'opening-type' => 'normal',
     ],
-    'status'     => true,
+    'status'     => false,
     'access_ps'  => ['1'],
     'settings'   => [
         'DuyuruWisecp_metin'            => "Ka\xc3\xa7\xc4\xb1r\xc4\xb1lmayacak f\xc4\xb1rsat! T\xc3\xbcm hosting paketlerinde b\xc3\xbcy\xc3\xbck indirim! Web sitenizi \xc5\x9fimdi g\xc3\xbcvenli ve h\xc4\xb1zl\xc4\xb1 hosting ile bulu\xc5\x9fturun, \xc3\xb6zel kampanyalar\xc4\xb1m\xc4\xb1zdan yararlan\xc4\xb1n. Detaylar i\xc3\xa7in hemen bize ula\xc5\x9f\xc4\xb1n!",
