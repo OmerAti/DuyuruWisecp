6e6c17f08ead4c81559d83bd96b11af56e611a9a
Ömer ATABER <67821076+OmerAti@users.noreply.github.com>
2024-08-26 13:42:34 +0300

    Add files via upload

---
+++ lang/en.php
@@ -0,0 +1,21 @@
+<?php 
+return [
+    'meta'     => [
+        'name'         => "AnnouncementWisecp Announcement Panel",
+        'descriptions' => "Adds a Modal Popup Announcement to your site.",
+    ],
+    'DuyuruWisecp_ust_name' => "Announcement Title",
+    'DuyuruWisecp_ust_description' => "The text that will appear as the title in the announcement modal.",
+    'DuyuruWisecp_resim_name' => "Announcement Image",
+    'DuyuruWisecp_resim_description' => "The full URL of the image that will appear in the announcement modal. (1021x1021 px recommended)",
+    'DuyuruWisecp_metin_name' => "Announcement Text",
+    'DuyuruWisecp_metin_description' => "The text that will appear in the announcement modal.",
+    'DuyuruWisecp_button_name' => "Button Link",
+    'DuyuruWisecp_button_description' => "The link to which the button in the announcement modal should redirect when clicked.",
+    'DuyuruWisecp_button_renk_name' => "Button Background Color Code",
+    'DuyuruWisecp_button_renk_description' => "Button Background Color Code.",
+    'DuyuruWisecp_button_renk_yazi_name' => "Button Text Color Code",
+    'DuyuruWisecp_button_renk_yazi_description' => "Button Text Color Code.",
+    'success1' => "Settings saved successfully.",
+    'error1' => "Announcement title cannot be empty.",
+];
---
+++ lang/tr.php
@@ -0,0 +1,21 @@
+<?php 
+return [
+    'meta'     => [
+        'name'         => "DuyuruWisecp Duyuru Paneli",
+        'descriptions' => "Sitenize Modal Popup Duyuru Ekler.",
+    ],
+    'DuyuruWisecp_ust_name' => "Duyuru Başlığı",
+    'DuyuruWisecp_ust_description' => "Duyuru modalında başlık olarak görünecek metin.",
+    'DuyuruWisecp_resim_name' => "Duyuru Görseli",
+    'DuyuruWisecp_resim_description' => "Duyuru modalında görünecek görselin tam bağlantısı. (1021x1021 px olarak önerilir)",
+    'DuyuruWisecp_metin_name' => "Duyuru Metni",
+    'DuyuruWisecp_metin_description' => "Duyuru modalında görünecek metin.",
+    'DuyuruWisecp_button_name' => "Buton Linki",
+    'DuyuruWisecp_button_description' => "Duyuru modalındaki butona tıklandığında yönlendirilmesi gereken bağlantı.",
+	'DuyuruWisecp_button_renk_name' => "Buton Arka Plan Renk Kodu.",
+	'DuyuruWisecp_button_renk_description' => "Buton Arka Plan Renk Kodu.",
+		'DuyuruWisecp_button_renk_yazi_name' => "Buton Yazı Renk Kodu.",
+	'DuyuruWisecp_button_renk_yazi_description' => "Buton Yazı Renk Kodu.",
+    'success1' => "Ayarlar başarıyla kaydedildi.",
+    'error1' => "Duyuru başlığı boş olamaz.",
+];
